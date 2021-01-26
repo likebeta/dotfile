dc118c28e06a5aca6a1faec630412f3f423c2d68
likebeta <ixxoo.me@gmail.com>
2021-01-26 17:43:24 +0800

    update

--- copy.sh
+++ copy.sh
@@ -4,10 +4,9 @@
 real_file_name=`readlink -f ${0}`
 SHELL_DIR=$(cd `dirname ${real_file_name}`; pwd)
 cd ${SHELL_DIR}
-tar --exclude=copy.sh --exclude=README.md --exclude=.bashrc --exclude=.git -cf - ./ | tar -xvf - -C ../
+tar --exclude=copy.sh --exclude=README.md --exclude=.bashrc --exclude=.git -cf - ./ | tar -xvf - -C ~/
 cat .bashrc >> ~/.bashrc
 
 echo "-----------------------------"
 echo "please exec: source ~/.bashrc"
 echo "-----------------------------"
-
