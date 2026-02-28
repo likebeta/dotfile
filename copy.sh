5a19c9badc6c1260cfb9aedfffaf36abb9e3ef33
蝎子 <ixxoo.me@gmail.com>
2026-02-28 16:28:15 +0800

    up
    Append command to source .bashrc in .bash_profile

--- copy.sh
+++ copy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # -*- coding=utf-8 -*-
 
+echo 'if [ -f ~/.bashrc ]; then source ~/.bashrc; fi' >> ~/.bash_profile
+
 real_file_name=`readlink -f ${0}`
 SHELL_DIR=$(cd `dirname ${real_file_name}`; pwd)
 cd ${SHELL_DIR}
